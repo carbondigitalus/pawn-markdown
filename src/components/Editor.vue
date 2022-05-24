8be7b6ef837266b7a6e7c4172e15b4f86810e43a
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-05-23 22:58:16 -0400

    lint file & add eslint comments as necessary

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -20,19 +20,14 @@ export default {
     EditorNewDiscussionButton,
   },
   computed: {
-    ...mapGetters('file', [
-      'isCurrentTemp',
-    ]),
-    ...mapGetters('layout', [
-      'styles',
-    ]),
-    ...mapGetters('data', [
-      'computedSettings',
-    ]),
+    ...mapGetters('file', ['isCurrentTemp']),
+    ...mapGetters('layout', ['styles']),
+    ...mapGetters('data', ['computedSettings']),
   },
   mounted() {
     const editorElt = this.$el.querySelector('.editor__inner');
-    const onDiscussionEvt = cb => (evt) => {
+    // eslint-disable-next-line arrow-parens
+    const onDiscussionEvt = (cb) => (evt) => {
       let elt = evt.target;
       while (elt && elt !== editorElt) {
         if (elt.discussionId) {
@@ -43,29 +38,41 @@ export default {
       }
     };
 
-    const classToggler = toggle => (discussionId) => {
-      editorElt.getElementsByClassName(`discussion-editor-highlighting--${discussionId}`)
-        .cl_each(elt => elt.classList.toggle('discussion-editor-highlighting--hover', toggle));
-      document.getElementsByClassName(`comment--discussion-${discussionId}`)
-        .cl_each(elt => elt.classList.toggle('comment--hover', toggle));
+    // eslint-disable-next-line arrow-parens
+    const classToggler = (toggle) => (discussionId) => {
+      editorElt
+        .getElementsByClassName(`discussion-editor-highlighting--${discussionId}`)
+        // eslint-disable-next-line arrow-parens
+        .cl_each((elt) => elt.classList.toggle('discussion-editor-highlighting--hover', toggle));
+      document
+        .getElementsByClassName(`comment--discussion-${discussionId}`)
+        // eslint-disable-next-line arrow-parens
+        .cl_each((elt) => elt.classList.toggle('comment--hover', toggle));
     };
 
     editorElt.addEventListener('mouseover', onDiscussionEvt(classToggler(true)));
     editorElt.addEventListener('mouseout', onDiscussionEvt(classToggler(false)));
-    editorElt.addEventListener('click', onDiscussionEvt((discussionId) => {
-      store.commit('discussion/setCurrentDiscussionId', discussionId);
-    }));
+    editorElt.addEventListener(
+      'click',
+      onDiscussionEvt((discussionId) => {
+        store.commit('discussion/setCurrentDiscussionId', discussionId);
+      }),
+    );
 
     this.$watch(
       () => store.state.discussion.currentDiscussionId,
       (discussionId, oldDiscussionId) => {
         if (oldDiscussionId) {
-          editorElt.querySelectorAll(`.discussion-editor-highlighting--${oldDiscussionId}`)
-            .cl_each(elt => elt.classList.remove('discussion-editor-highlighting--selected'));
+          editorElt
+            .querySelectorAll(`.discussion-editor-highlighting--${oldDiscussionId}`)
+            // eslint-disable-next-line arrow-parens
+            .cl_each((elt) => elt.classList.remove('discussion-editor-highlighting--selected'));
         }
         if (discussionId) {
-          editorElt.querySelectorAll(`.discussion-editor-highlighting--${discussionId}`)
-            .cl_each(elt => elt.classList.add('discussion-editor-highlighting--selected'));
+          editorElt
+            .querySelectorAll(`.discussion-editor-highlighting--${discussionId}`)
+            // eslint-disable-next-line arrow-parens
+            .cl_each((elt) => elt.classList.add('discussion-editor-highlighting--selected'));
         }
       },
     );
