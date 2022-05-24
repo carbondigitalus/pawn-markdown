a85653c912b3af2fbaf29dfd371df96931247c70
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-05-23 22:57:47 -0400

    lint file & add eslint comments as necessary

--- src/components/gutters/PreviewNewDiscussionButton.vue
+++ src/components/gutters/PreviewNewDiscussionButton.vue
@@ -15,18 +15,13 @@ export default {
     coordinates: null,
   }),
   methods: {
-    ...mapActions('discussion', [
-      'createNewDiscussion',
-    ]),
+    ...mapActions('discussion', ['createNewDiscussion']),
     checkSelection() {
       clearTimeout(this.timeout);
       this.timeout = setTimeout(() => {
         let offset;
         // Show the button if content is not a revision and preview selection is not empty
-        if (
-          !store.state.content.revisionContent &&
-          editorSvc.previewSelectionRange
-        ) {
+        if (!store.state.content.revisionContent && editorSvc.previewSelectionRange) {
           this.selection = editorSvc.getTrimmedSelection();
           if (this.selection) {
             const { text } = editorSvc.previewCtxWithDiffs;
@@ -36,19 +31,14 @@ export default {
             }
           }
         }
-        this.coordinates = offset
-          ? editorSvc.getPreviewOffsetCoordinates(offset)
-          : null;
+        this.coordinates = offset ? editorSvc.getPreviewOffsetCoordinates(offset) : null;
       }, 25);
     },
   },
   mounted() {
     this.$nextTick(() => {
       editorSvc.$on('previewSelectionRange', () => this.checkSelection());
-      this.$watch(
-        () => store.getters['layout/styles'].previewWidth,
-        () => this.checkSelection(),
-      );
+      this.$watch(() => store.getters['layout/styles'].previewWidth, () => this.checkSelection());
       this.checkSelection();
     });
   },
