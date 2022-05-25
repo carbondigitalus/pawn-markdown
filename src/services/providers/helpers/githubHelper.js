7cd76c034cce878bd92b922fce2ae92647a0bc6c
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-05-24 23:57:30 -0400

    lint file

--- src/services/providers/helpers/githubHelper.js
+++ src/services/providers/helpers/githubHelper.js
@@ -4,25 +4,26 @@ import store from '../../../store';
 import userSvc from '../../userSvc';
 import badgeSvc from '../../badgeSvc';
 
-const getScopes = token => [token.repoFullAccess ? 'repo' : 'public_repo', 'gist'];
+const getScopes = (token) => [token.repoFullAccess ? 'repo' : 'public_repo', 'gist'];
 
-const request = (token, options) => networkSvc.request({
-  ...options,
-  headers: {
-    ...options.headers || {},
-    Authorization: `token ${token.accessToken}`,
-  },
-  params: {
-    ...options.params || {},
-    t: Date.now(), // Prevent from caching
-  },
-});
+const request = (token, options) =>
+  networkSvc.request({
+    ...options,
+    headers: {
+      ...(options.headers || {}),
+      Authorization: `token ${token.accessToken}`,
+    },
+    params: {
+      ...(options.params || {}),
+      t: Date.now(), // Prevent from caching
+    },
+  });
 
-const repoRequest = (token, owner, repo, options) => request(token, {
-  ...options,
-  url: `https://api.github.com/repos/${encodeURIComponent(owner)}/${encodeURIComponent(repo)}/${options.url}`,
-})
-  .then(res => res.body);
+const repoRequest = (token, owner, repo, options) =>
+  request(token, {
+    ...options,
+    url: `https://api.github.com/repos/${encodeURIComponent(owner)}/${encodeURIComponent(repo)}/${options.url}`,
+  }).then((res) => res.body);
 
 const getCommitMessage = (name, path) => {
   const message = store.getters['data/computedSettings'].git[name];
@@ -127,12 +128,7 @@ export default {
    * https://developer.github.com/v3/repos/commits/#get-a-single-commit
    * https://developer.github.com/v3/git/trees/#get-a-tree
    */
-  async getTree({
-    token,
-    owner,
-    repo,
-    branch,
-  }) {
+  async getTree({ token, owner, repo, branch }) {
     const { commit } = await repoRequest(token, owner, repo, {
       url: `commits/${encodeURIComponent(branch)}`,
     });
@@ -148,13 +144,7 @@ export default {
   /**
    * https://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
    */
-  async getCommits({
-    token,
-    owner,
-    repo,
-    sha,
-    path,
-  }) {
+  async getCommits({ token, owner, repo, sha, path }) {
     return repoRequest(token, owner, repo, {
       url: 'commits',
       params: { sha, path },
@@ -165,15 +155,7 @@ export default {
    * https://developer.github.com/v3/repos/contents/#create-a-file
    * https://developer.github.com/v3/repos/contents/#update-a-file
    */
-  async uploadFile({
-    token,
-    owner,
-    repo,
-    branch,
-    path,
-    content,
-    sha,
-  }) {
+  async uploadFile({ token, owner, repo, branch, path, content, sha }) {
     return repoRequest(token, owner, repo, {
       method: 'PUT',
       url: `contents/${encodeURIComponent(path)}`,
@@ -189,14 +171,7 @@ export default {
   /**
    * https://developer.github.com/v3/repos/contents/#delete-a-file
    */
-  async removeFile({
-    token,
-    owner,
-    repo,
-    branch,
-    path,
-    sha,
-  }) {
+  async removeFile({ token, owner, repo, branch, path, sha }) {
     return repoRequest(token, owner, repo, {
       method: 'DELETE',
       url: `contents/${encodeURIComponent(path)}`,
@@ -211,13 +186,7 @@ export default {
   /**
    * https://developer.github.com/v3/repos/contents/#get-contents
    */
-  async downloadFile({
-    token,
-    owner,
-    repo,
-    branch,
-    path,
-  }) {
+  async downloadFile({ token, owner, repo, branch, path }) {
     const { sha, content } = await repoRequest(token, owner, repo, {
       url: `contents/${encodeURIComponent(path)}`,
       params: { ref: branch },
@@ -232,49 +201,43 @@ export default {
    * https://developer.github.com/v3/gists/#create-a-gist
    * https://developer.github.com/v3/gists/#edit-a-gist
    */
-  async uploadGist({
-    token,
-    description,
-    filename,
-    content,
-    isPublic,
-    gistId,
-  }) {
-    const { body } = await request(token, gistId ? {
-      method: 'PATCH',
-      url: `https://api.github.com/gists/${gistId}`,
-      body: {
-        description,
-        files: {
-          [filename]: {
-            content,
+  async uploadGist({ token, description, filename, content, isPublic, gistId }) {
+    const { body } = await request(
+      token,
+      gistId
+        ? {
+            method: 'PATCH',
+            url: `https://api.github.com/gists/${gistId}`,
+            body: {
+              description,
+              files: {
+                [filename]: {
+                  content,
+                },
+              },
+            },
+          }
+        : {
+            method: 'POST',
+            url: 'https://api.github.com/gists',
+            body: {
+              description,
+              files: {
+                [filename]: {
+                  content,
+                },
+              },
+              public: isPublic,
+            },
           },
-        },
-      },
-    } : {
-      method: 'POST',
-      url: 'https://api.github.com/gists',
-      body: {
-        description,
-        files: {
-          [filename]: {
-            content,
-          },
-        },
-        public: isPublic,
-      },
-    });
+    );
     return body;
   },
 
   /**
    * https://developer.github.com/v3/gists/#get-a-single-gist
    */
-  async downloadGist({
-    token,
-    gistId,
-    filename,
-  }) {
+  async downloadGist({ token, gistId, filename }) {
     const result = (await request(token, {
       url: `https://api.github.com/gists/${gistId}`,
     })).body.files[filename];
@@ -287,10 +250,7 @@ export default {
   /**
    * https://developer.github.com/v3/gists/#list-gist-commits
    */
-  async getGistCommits({
-    token,
-    gistId,
-  }) {
+  async getGistCommits({ token, gistId }) {
     const { body } = await request(token, {
       url: `https://api.github.com/gists/${gistId}/commits`,
     });
@@ -300,12 +260,7 @@ export default {
   /**
    * https://developer.github.com/v3/gists/#get-a-specific-revision-of-a-gist
    */
-  async downloadGistRevision({
-    token,
-    gistId,
-    filename,
-    sha,
-  }) {
+  async downloadGistRevision({ token, gistId, filename, sha }) {
     const result = (await request(token, {
       url: `https://api.github.com/gists/${gistId}/${sha}`,
     })).body.files[filename];
